1df632fb98604b88ac0798b287228fad19b1df91
lingxue <info@myxxjs.com>
2017-06-22 00:04:39 -0700

    update gps batch

--- config/Stg_SystemConfig.js
+++ config/Stg_SystemConfig.js
@@ -28,5 +28,12 @@ const mongoConfig = {
     connect : 'mongodb://127.0.0.1:27017/log_records'
 }
 
+const gpsHost = {
+    host : '139.224.65.40',
+    port : 80,
+    cookie :'ASP.NET_SessionId=cmgw4jbltlxwxm55kjpxnn55',
+    bizTruckUrl : '/webgps/ajax/VehicleMonitor,App_Web_ecpd3mt7.ashx?_method=GetVehJson&_session=r',
+    truckBaseUrl:'/webgps/ajax/NetUI_BaiduMap,App_Web_askdqdzp.ashx?_method=GetVehicleRef&_session=r'
+}
 
-module.exports = { mysqlConnectOptions ,loggerConfig, logLevel , mongoConfig  }
+module.exports = { mysqlConnectOptions ,loggerConfig, logLevel , mongoConfig  ,gpsHost }
