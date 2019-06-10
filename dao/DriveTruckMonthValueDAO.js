6bb88d9bf85809bb62f4e718c196148d8339e1a5
maguon <info@myxxjs.com>
2019-06-10 14:23:26 +0800

    Add month value batch

--- dao/DriveTruckMonthValueDAO.js
+++ dao/DriveTruckMonthValueDAO.js
@@ -15,14 +15,15 @@ function addDistance(params,callback){
         " sum(case when dpr.load_flag = 1 then dpr.distance end) as load_distance, " +
         " sum(case when dpr.load_flag = 0 then dpr.distance end) as no_load_distance, " +
         " sum(dpr.distance) as distance, " +
-        " sum( case when dprl.receive_flag=0 and dprl.transfer_flag=0 then dpr.car_count end) not_storage_car_count, " +
-        " sum( case when dprl.receive_flag=1 or dprl.transfer_flag=1 then dpr.car_count end) storage_car_count, " +
-        " sum( case when dprl.receive_flag=0 and dprl.transfer_flag=0 then dpr.car_count end)*4 as enter_fee " +
+        " sum( case when dprl.receive_flag=0 and dprl.transfer_flag=0 then dprl.real_count end) not_storage_car_count, " +
+        " sum( case when dprl.receive_flag=1 or dprl.transfer_flag=1 then dprl.real_count end) storage_car_count, " +
+        " sum( case when dprl.receive_flag=0 and dprl.transfer_flag=0 then dprl.real_count end)*4 as enter_fee " +
         " from dp_route_task dpr " +
         " left join truck_info t on dpr.truck_id = t.id " +
         " left join truck_brand tb on t.brand_id = tb.id " +
         " left join dp_route_load_task dprl on dpr.id = dprl.dp_route_task_id " +
-        " where dpr.task_status >=9 and dpr.task_plan_date>="+params.yMonth+"01 and dpr.task_plan_date<=" +params.yMonth+"31"+
+        " where dpr.task_status >=9 " +
+        " and dpr.task_plan_date>="+params.yMonth+"01 and dpr.task_plan_date<=" +params.yMonth+"31"+
         " group by dpr.drive_id,dpr.truck_id ";
     var paramsArray=[],i=0;
 
@@ -41,8 +42,9 @@ function updateOutput(params,callback){
         " left join car_info ci on drltd.car_id = ci.id " +
         " left join entrust_city_route_rel ecrr on ci.entrust_id = ecrr.entrust_id " +
         " and ci.make_id = ecrr.make_id and ci.route_id = ecrr.city_route_id " +
-        " and ci.size_type =ecrr.size_type  where dprt.task_plan_date>= "+params.yMonth+"01"+
-        " and dprt.task_plan_date<="+params.yMonth+"31 and dprt.task_status >=9 " +
+        " and ci.size_type =ecrr.size_type  " +
+        " where dprt.task_plan_date>= "+params.yMonth+"01 and dprt.task_plan_date<="+params.yMonth+"31 " +
+        " and dprt.task_status >=9 " +
         " group by dprt.drive_id,dprt.truck_id) dprm " +
         " on dtmv.drive_id = dprm.drive_id and dtmv.truck_id = dprm.truck_id " +
         " and dtmv.y_month = "+params.yMonth+" set dtmv.output = dprm.output ";
@@ -92,7 +94,8 @@ function updateDistanceSalary(params,callback){
         " end) distance_salary, " +
         " sum(case when dpr.reverse_flag=1 then dpr.reverse_money end) reverse_salary " +
         " from dp_route_task dpr " +
-        " where dpr.task_plan_date>="+params.yMonth+"01 and dpr.task_plan_date<="+params.yMonth+"31 and dpr.task_status>=9 " +
+        " where dpr.task_plan_date>="+params.yMonth+"01 and dpr.task_plan_date<="+params.yMonth+"31 " +
+        " and dpr.task_status>=9 " +
         " group by dpr.drive_id,dpr.truck_id) dprm " +
         " on dtmv.drive_id = dprm.drive_id and dtmv.truck_id = dprm.truck_id " +
         " and dtmv.y_month = "+params.yMonth+" set dtmv.distance_salary = dprm.distance_salary , dtmv.reverse_salary = dprm.reverse_salary ";
@@ -226,12 +229,14 @@ function updateRepair(params,callback){
 
 function updateCarOilFee(params,callback){
     var query = " update drive_truck_month_value dtmv inner join( " +
-        " select dprtf.drive_id,dprtf.truck_id,sum(dprtf.car_oil_fee) car_oil_fee ,sum(dprtf.total_price) truck_parking_fee " +
+        " select dprtf.drive_id,dprtf.truck_id,sum(dprtf.car_oil_fee) car_oil_fee,sum(dprtf.car_total_price) car_parking_total_fee," +
+        " sum(dprtf.total_price) truck_parking_fee " +
         " from dp_route_task_fee dprtf " +
         " where dprtf.date_id>="+params.yMonth+"01 and dprtf.date_id<="+params.yMonth+"31 and dprtf.status=2 " +
         " group by dprtf.drive_id,dprtf.truck_id) dprtfm " +
         " on dtmv.drive_id = dprtfm.drive_id and dtmv.truck_id = dprtfm.truck_id and dtmv.y_month = " +params.yMonth+
-        " set dtmv.car_oil_fee = dprtfm.car_oil_fee , dtmv.truck_parking_fee = dprtfm.truck_parking_fee ";
+        " set dtmv.car_oil_fee = dprtfm.car_oil_fee , dtmv.car_parking_total_fee = dprtfm.car_parking_total_fee , " +
+        " dtmv.truck_parking_fee = dprtfm.truck_parking_fee ";
     var paramsArray=[],i=0;
 
     db.dbQuery(query,paramsArray,function(error,rows){
