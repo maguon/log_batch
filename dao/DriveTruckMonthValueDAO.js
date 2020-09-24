e2d8bdd079d18f7fc6ccb8820245f5e5640374b6
maguon <info@myxxjs.com>
2020-09-24 15:06:27 +0800

    reset drive exceed oil gps money

--- dao/DriveTruckMonthValueDAO.js
+++ dao/DriveTruckMonthValueDAO.js
@@ -145,6 +145,7 @@ function updateDamage(params,callback){
 function updateCleanFee(params,callback){
 
     var lastDay = moment(params.yMonth+'01').endOf('month').format("YYYYMMDD");
+    var lastDateTime = moment(params.yMonth+'01').endOf('month').format("YYYY-MM-DD") +' 23:59:59';
     var query = " update drive_truck_month_value dtmv inner join( " +
         " select drcr.drive_id,drcr.truck_id," +
         " sum(drcr.total_price) total_clean_fee, " +
@@ -153,7 +154,7 @@ function updateCleanFee(params,callback){
         " sum(drcr.total_run_fee) total_run_fee, " +
         " sum(drcr.lead_fee) lead_fee " +
         " from dp_route_load_task_clean_rel drcr " +
-        " where drcr.created_on>="+params.yMonth+"01 and drcr.created_on<="+lastDay+" and drcr.status=2 " +
+        " where drcr.created_on>="+params.yMonth+"01 and drcr.created_on<="+lastDateTime+" and drcr.status=2 " +
         " group by drcr.drive_id,drcr.truck_id) drcrm " +
         " on dtmv.drive_id = drcrm.drive_id and dtmv.truck_id = drcrm.truck_id and dtmv.y_month = " +params.yMonth+
         " set dtmv.clean_fee = drcrm.total_clean_fee , dtmv.trailer_fee = drcrm.total_trailer_fee , " +
