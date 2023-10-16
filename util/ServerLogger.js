68dc7f4d170d6f27cdef42e8622950b07d03b3bc
maguon <>
2023-10-16 15:32:46 +0800

    update log level

--- util/ServerLogger.js
+++ util/ServerLogger.js
@@ -4,7 +4,7 @@ const log4js = require('log4js');
 const createLogger = (name) => {
     log4js.configure(sysConfig.loggerConfig);
     const logger = log4js.getLogger(name);
-    logger.setLevel(sysConfig.logLevel);
+    //logger.setLevel(sysConfig.logLevel);
     return logger;
 }
 
